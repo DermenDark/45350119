49048ffacda598f1f0173558d693b73b0d4bc98b
DermenDark <volodya.shirko@gmail.com>
2024-09-23 18:01:29 +0300

    Mu cpp

--- 453505/ОАиП/LR1./Untitled-1.cpp
+++ 453505/ОАиП/LR1./Untitled-1.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
-// ааааа
+#include <cmath> // Для функции sin
+
 int main() {
-    Double touble, start, end, step;
-    std::cout <<  "Введите начальное значение x: ";
-    std::cin << start; // Задаем исходное значение переменной
+    double start, end, step;
+
+    std::cout << "Введите начальное значение x: ";
+    std::cin >> start; // Задаем исходное значение переменной
+
     std::cout << "Введите конечное значение x: ";
-    std::cin << end; // Задаем конечно значение переменной
+    std::cin >> end; // Задаем конечное значение переменной
+
     std::cout << "Введите шаг: ";
-    std::cin << step; // Задаем шаг, с которым с которым будет изменяться аргумент
+    std::cin >> step; // Задаем шаг, с которым будет изменяться аргумент
+
     for (double x = start; x <= end; x += step) {
         double y = sin(x); // функция y = sin(x)
         std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
     }
-    return nullptr;
+
+    return 0;
 }
