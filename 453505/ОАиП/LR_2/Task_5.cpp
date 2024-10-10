bcfdc7d9515df0503497b1ae7ecc8b723f6e09c9
DermenDark <volodya.shirko@gmail.com>
2024-10-10 10:30:47 +0300

    Task_5

---
+++ 453505/ОАиП/LR_2/Task_5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    float x=30, y=32;
+    
+    if( x>y ){
+        std::cout << "x > y ";}
+    else{
+        std::cout << "x < y ";}
+    
+    std::cout << "\nзначение x: "<<x<< "\nзначение y: "<<y;
+    return 0;
+}
