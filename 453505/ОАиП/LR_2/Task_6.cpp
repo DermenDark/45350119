2b7c765303937daad6036218b1ede8acd3431617
DermenDark <volodya.shirko@gmail.com>
2024-10-10 11:31:28 +0300

    finali

--- 453505/ОАиП/LR_2/Task_6.cpp
+++ 453505/ОАиП/LR_2/Task_6.cpp
@@ -3,24 +3,28 @@
 
 
 int main() {
-    double x,y,a,b,c,d,k;
+    double x,y,a,b,z,c,d,k,p;
 
     std::cout << "Введите значение x: ";
     std::cin >> x;
+    std::cout << "Введите значение z: ";
+    std::cin >> z;
 
     std::cout << "Введите значение k: ";
     std::cin >> k;
     std::cout << "Введите значение c: ";
     std::cin >> c;
-    
     std::cout << "Введите значение d: ";
     std::cin >> d;
+    std::cout << "Введите значение p: ";
+    std::cin >> p;
+
 
     
-    a= log10(x);
-    b= (x+ exp(d));
+    a= (sin(x*(180/3.141592)-z));
+    b= fabs(p-x);
     
-    y= ((a+b)- (c*c)/k);
+    y= ((a+b)*(a+b)- k/(c*d));
     
     std::cout << "Получено значение y: "<<y;
 
