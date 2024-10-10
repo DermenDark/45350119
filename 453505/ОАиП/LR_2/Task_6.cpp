e3ca4d90a1cb269ecaae9b80bb99373139719c25
DermenDark <volodya.shirko@gmail.com>
2024-10-10 10:49:43 +0300

    finali

--- 453505/ОАиП/LR_2/Task_6.cpp
+++ 453505/ОАиП/LR_2/Task_6.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <cmath>
+
 
 int main() {
-    double x,y,z;
+    double x,y,s,a,b,d;
 
     std::cout << "Введите значение x: ";
     std::cin >> x;
     std::cout << "Введите значение y: ";
     std::cin >> y;
-    std::cout << "Введите значение z: ";
-    std::cin >> z;
+    std::cout << "Введите значение d: ";
+    std::cin >> d;
+    
+    b= (x+d);
+    a= ((d*x)/b);
+    s= ( (a*a + b*cos(x*(180/3.141592)) )/( pow(d,3) + (a + d - b) ) );
+    
+    std::cout << "Получено значение s: "<<s;
 
-    if (x <y+z && y <x+z && z <y+x) {
-        std::cout << "треугольник существует! ";
-    }
-    else{
-        std::cout << "нет такого треугольника ";
-    }
     return 0;
 }
+
