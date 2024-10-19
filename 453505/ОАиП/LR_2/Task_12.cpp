c8aa1ecbd41446576161e58623674c24770d97a9
DermenDark <volodya.shirko@gmail.com>
2024-10-19 15:45:12 +0300

    Task12

--- 453505/ОАиП/LR_2/Task_12.cpp
+++ 453505/ОАиП/LR_2/Task_12.cpp
@@ -1,51 +1,77 @@
 #include <iostream>
-#include <cmath>
+double sqrt(double number) {
+    double x = number;
+    double y = 1.0;
+    double e = 0.0000001; // точность
 
-double factorial(int n) {
-    double res = 1;
-    for (int i = 1; i <= n; i++) {
-        res *= i;
+    while (x - y > e) {
+        x = (x + y) / 2;
+        y = number / x;
     }
-    return res;
+    return x;
 }
-double pooow(double osnov,int step){
-    double res=1;
-    for(int n=0;n<step; n++){
-        res *= osnov;
-        
-    }
-    return res;
-}
-double sinn(double kor){
-    double x_2;
 
-    x_2= (kor / 6.28318530716);
-    int x_3 = (kor / 6.28318530716);
+int main()
+{   int a,b,c;
+    double x_1=0,x_2=0,x_3=0,x_4=0;
     
-    double ress = (x_2-x_3)*6.28318530716;
+    std::cin >> a>>b>>c;
+    // std::cout<< a<< b<< c;
+    
+    double d = (b*b-4*a*(c - 2*a));
 
-    double res=0,prom;
-    for(int n=1; n<70;n++){
-        prom= pooow(ress,(2*n-1))/factorial(2*n-1);
-        if (n % 2 != 1) {
-            prom= -prom;
+    if(d>0){
+        
+        double t_1=(-b+sqrt(d))/(2*a);
+        double t_2=(-b-sqrt(d))/(2*a);
+
+        int a=1,c=1;
+        double d_1 = (t_1*t_1-4*a*c);
+        double d_2 = (t_2*t_2-4*a*c);
+        
+        if(d_1>0){ 
+            
+            x_1 = (t_1+sqrt(d_1))/(2*a);
+            x_2 = (t_1-sqrt(d_1))/(2*a);
+            std::cout<<"\nx1= "<< x_1<<"\n x2= "<<x_2;
         }
-        std::cout << "\nRes: "<< n<<"-("<<prom;
-        res += prom;
+        else if(d_1=0){
+            x_1 = (t_1)/2*a;
+            std::cout<<"\n x1= "<<x_1;
+        }
+        else{
+            std::cout<< "\nнет корней от t_1";
+        }
+        
+        if(d_2>0){
+            
+            x_3 =(t_2+sqrt(d_2))/(2*a);
+            x_4 =(t_2-sqrt(d_2))/(2*a);
+            std::cout<<" \nx3= "<<x_3<<" \nx4= "<<x_4;
+        }
+        else if(d_1=0){
+            x_1 = (t_1)/2*a;
+            std::cout<<" \nx3= "<<x_1;
+        }
+        else{
+            std::cout<< "\nнет корней от t_2";
+        }
     }
-
-    return res;
-}
-
-int main() {
-    double x=0,result_1=0,res;
-    int massiv[4]={8,32,69,23};
-
-    std::cout << "x: ";
-    std::cin >> x;
-  
-    result_1=sinn(x);
-    res = sin(x);
-    std::cout << "\nRes_1= "<< result_1<<"\nres="<<res;
+    else if(d=0){
+        double t_1=(-b)/2*a;
+        
+        if(t_1>0){ int a=1,c=1; 
+            double d_2 = (t_1*t_1-4*a*(c-2*a));
+            
+            x_1 = (t_1+sqrt(d_2))/(2*a);
+            x_2 = (t_1-sqrt(d_2))/(2*a);
+            std::cout<<"x= "<< x_1<<" x= "<<x_2;
+        }
+        else{std::cout<< "\nнет корней";}
+    }
+    else{
+        std::cout<< "\nнет корней";
+    }
+        
     return 0;
 }
