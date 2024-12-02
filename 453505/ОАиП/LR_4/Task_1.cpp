ee522296fbdc7c45c1ca0071ec1390cf3bcdb55b
DermenDark <volodya.shirko@gmail.com>
2024-12-02 21:52:32 +0300

    task_1

--- 453505/ОАиП/LR_4/Task_1.cpp
+++ 453505/ОАиП/LR_4/Task_1.cpp
@@ -1,20 +1,61 @@
+#include <sstream>  
+#include <vector>
 #include <iostream>
 
-int main()
-{   int i=1,a,b;
-    float N;
-    while(i<=30){
-        if(i % 2==1){
-            a = i;
-            b = i*i;
-        }
-        else{
-            a = i/2;
-            b = i*i*i;
-        }
-        N +=(a-b)*(a-b);
-        i++;
+void massiv() {
+    std::vector<long double> vec;  
+    std::string input;             
+    bool no_num = true;
+    bool proverka = true;
+    
+    std::cout << "\nВведите массив чисел (разделяйте числа пробелами): ";
+    std::getline(std::cin, input);  
+
+    std::stringstream ss(input);  
+    long double num;
+    
+
+    while (ss >> num) {
+        vec.push_back(num);
     }
-    std::cout<<"|| "<<N<<"|| ";
+
+    if (ss.fail() && !ss.eof()) {
+        std::cout << "Ошибка.Были введены элементы, не являющиеся числами.\n";
+        return;
+    }
+
+
+    for (size_t i = 0; i < vec.size() - 1; ++i) {
+        if (vec[i] < vec[i + 1]) {  
+            proverka = false;
+            break;
+        }
+    }
+    
+    if (proverka) {
+        std::cout << "Массив отсортирован.\n";
+    } else {
+        std::cout << "Массив не отсортирован.\n";
+    }
+
+    std::cout << "Массив: ";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        std::cout << vec[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    char otvet;
+    std::cout << "Определить, упорядочены ли элементы массива по убыванию.\nВыполнил задание:Ширко Владимир.\nВариант-№4(19 в списке)\n";
+    do {
+        massiv();
+        std::cout << "\nХотите ввести еще один массив чисел? (y-да/n-нет): ";
+        std::cin >> otvet;
+        std::cin.ignore();
+    } while (otvet == 'y' || otvet == 'Y');
+    
+    std::cout << "Программа завершена.\n";
+    
     return 0;
 }
