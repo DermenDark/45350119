3b8523d23baeffddf31f2835e31572f9d9f38dbd
DermenDark <volodya.shirko@gmail.com>
2024-12-03 03:12:47 +0300

    подпроекты

---
+++ 453505/ОАиП/LR_4/LabWork4_PartC./main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(int, char**){
+    printf("Hello, from LABWORK$_PARTC!\n");
+}
--- 453505/ОАиП/LR_4/tempCodeRunnerFile.cpp
+++
@@ -1,5 +0,0 @@
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j){
-        std::cin>>arr[i][j];
-        }
-    }
